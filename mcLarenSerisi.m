79a97216d44db37bc7c1c8c6f920d3a7445cbaf1
Mustafa YILDIRIM <mustafa.yildirim1997@gmail.com>
2019-02-28 11:35:19 +0300

    McLarenSerisi yaklaşım

---
+++ mcLarenSerisi.m
@@ -0,0 +1,11 @@
+a1 = 0; a0 = 0; Terim = 0;
+x = 0.5; n = 0; gercekBagilYuzdeHata = 5e-4; yaklasimHata = 5e-3;
+
+while yaklasimHata > gercekBagilYuzdeHata
+    Terim=(x^n)/factorial(n)
+    a1=a0+Terim
+    yaklasimHata = 100*abs((a1-a0)/a1)
+    a0 = a1;
+    n=n+1;
+end
+n
