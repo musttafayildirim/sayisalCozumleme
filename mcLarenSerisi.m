adc33782a9d51f59cd3051965c06c9a230453c06
Mustafa YILDIRIM <mustafa.yildirim1997@gmail.com>
2019-02-28 11:58:23 +0300

    message

--- mcLarenSerisi.m
+++ mcLarenSerisi.m
@@ -1,11 +1,12 @@
+clc; clear all;
 a1 = 0; a0 = 0; Terim = 0;
 x = 0.5; n = 0; gercekBagilYuzdeHata = 5e-4; yaklasimHata = 5e-3;
 
 while yaklasimHata > gercekBagilYuzdeHata
-    Terim=(x^n)/factorial(n)
-    a1=a0+Terim
-    yaklasimHata = 100*abs((a1-a0)/a1)
+    Terim=(x^n)/factorial(n);
+    a1=a0+Terim;
+    yaklasimHata = 100*abs((a1-a0)/a1);
     a0 = a1;
     n=n+1;
+    display( "Adým : " + n + "  Bulunan Deger : " + a1)
 end
-n
